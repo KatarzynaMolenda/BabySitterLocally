1a051dc3e5e91428694c68b9f1f6a3ff4ac96dad
KatarzynaMolenda <67151343+KatarzynaMolenda@users.noreply.github.com>
2020-12-15 19:19:53 +0100

    adding content to folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,50 +7,6 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-
-  constructor(private httpClient: HttpClient) {
-  }
-
-  newBabysitter: Babysitter = {
-    id: 0,
-    firstName: '',
-    lastName: '',
-    postalCode: '',
-
-  };
-
-  babysitters: Babysitter[] = [{
-    firstName: 'Kasia',
-    lastName: 'Molenda',
-    postalCode: '03-138'
-  },
-    {
-      firstName: 'Marcin',
-      lastName: 'Nowak',
-      postalCode: '03-252'
-    }];
-
-  private url = 'http://localhost:8080/babysitter';
-
-  ngOnInit(): void {
-    this.loadData();
-  }
-
-  private loadData(): void {
-    this.httpClient.get<Babysitter[]>(this.url)
-      .subscribe(babysitters => this.babysitters = babysitters);
-  }
-
-  delete(id: number): void {
-    this.httpClient.delete(this.url + '/' + id)
-      .subscribe(() => this.loadData());
-  }
-
-  createBabysitter(): void {
-    this.httpClient.post<Babysitter>(this.url, this.newBabysitter)
-      .subscribe(babysitter => this.babysitters.push());
-
-  }
+export class AppComponent {
+  title: 'BabySitterLocally';
 }
