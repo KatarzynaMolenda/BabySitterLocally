522caf308ac0580340119814cc4d3b176f2ca7a8
Pawel Buczek <pawel.buczek@email.com>
2021-01-21 19:01:57 +0100

    reminding password added

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,18 +1,23 @@
 import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Child} from "../model/child";
 
 @Component({
   selector: 'app-register',
   templateUrl: './forgot-password.component.html'
 })
 export class ForgotPasswordComponent {
+  email: string;
 
   constructor(public httpClient: HttpClient, private activeRoute: ActivatedRoute, private router: Router) {
+    this.email = '';
   }
 
   onFormSubmit(): void {
     console.log('Submitted Forgot Password Form');
+    this.httpClient.get('http://localhost:8080/mailer/RemindPassword/' + this.email)
+      .subscribe();
     this.router.navigateByUrl('/login');
   }
 
